d29a33ed37291efe3bc825a8dbd29e12e36528ce
NavedCoded <navecoded@gmail.com>
2016-05-16 21:05:49 +0100

    Testing brought both notifications at the same time. Changed into two functions for now.

--- eyecare.py
+++ eyecare.py
@@ -7,6 +7,7 @@ def eyeuse():
     s.call(['notify-send', 'Your Eyes', 'Take a break. Just 20 seconds.', '--urgency=critical'])
     eyecare()
 
+
 def eyecare():
     time.sleep(22)
     s.call(['notify-send', 'Good Job', 'You can get back to work.'])
