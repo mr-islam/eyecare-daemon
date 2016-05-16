1d976568b3109785f5f30a2907233ff3a40d1ef2
NavedCoded <navecoded@gmail.com>
2016-05-16 20:01:38 +0100

    Testing brought both notifications at the same time. Changed into two functions for now.

--- eyecare.py
+++ eyecare.py
@@ -2,11 +2,14 @@ import subprocess as s
 import time
 
 
-def eyecare():
+def eyeuse():
     time.sleep(1200)
     s.call(['notify-send', 'Your Eyes', 'Take a break. Just 20 seconds.', '--urgency=critical'])
-    time.sleep(22)
-    s.call(['notify-send', 'Good Job', 'You can get back to work.'])
     eyecare()
 
-eyecare()
+def eyecare():
+    time.sleep(22)
+    s.call(['notify-send', 'Good Job', 'You can get back to work.'])
+    eyeuse()
+
+eyeuse()
