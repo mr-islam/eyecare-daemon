eaa7c13770db6ad5729fbcf34f94b7fe24de04bd
NavedCoded <navecoded@gmail.com>
2016-06-17 18:53:39 +0100

    cleaner urgency

--- eyecare.py
+++ eyecare.py
@@ -8,14 +8,14 @@ command = 'notify-send'
 warning_title = 'Your eyes'
 warning_message = 'Take a break. Just 20 seconds.'
 
-#notify-send parameters. system default is 2.
-priority1 = 'low'
-priority2 = 'normal'
-priority3 = 'critical'
+#notify-send importance parameters. system default is 2.
+priority1 = '-u low'
+priority2 = '-u normal'
+priority3 = '-u critical'
 
 def warning():
     time.sleep(1200) #time in seconds
-    s.call([command, warning_title, warning_message, '--urgency=' + priority3]) #critical because eyes are important. And also for the different color
+    s.call([command, warning_title, warning_message, priority3]) #critical because eyes are important. And also for the different color
     success()
 
 #change to your liking
@@ -24,9 +24,9 @@ success_message = 'You can get back to work.'
 
 def success():
     time.sleep(22) #22 instead of 20 just to be sure
-    s.call([command, success_title, success_message, '--urgency=' + priority2])
+    s.call([command, success_title, success_message, priority2])
     warning()
 
 warning()
 
-#TODO: osx support, custom timeout
+#TODO: osx support
