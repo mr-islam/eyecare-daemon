01d39ecb8f5b482943291409529425516f87d640
NavedCoded <navecoded@gmail.com>
2016-06-17 15:00:41 +0100

    Variable names succeful

--- eyecare.py
+++ eyecare.py
@@ -4,13 +4,13 @@ import time
 eyeuse_title = 'Your eyes'
 eyeuse_message = 'Take a break. Just 20 seconds.'
 
+eyecare_title = 'Good job'
+eyecare_message = 'You can get back to work.'
+
 def eyeuse():
     time.sleep(1200)
     s.call(['notify-send', eyeuse_title, eyeuse_message, '--urgency=critical'])
     eyecare()
-
-eyecare_title = 'Good job'
-eyecare_message = 'You can get back to work.'
 
 def eyecare():
     time.sleep(22)
@@ -18,3 +18,4 @@ def eyecare():
     eyeuse()
 
 eyeuse()
+
