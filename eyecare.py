15839ef47048ccafcea433ab5c5f5729fd7454f5
Naved Islam <navedcoded@gmail.com>
2016-06-17 13:20:06 +0100

    Variable messages added fully

--- eyecare.py
+++ eyecare.py
@@ -9,12 +9,12 @@ def eyeuse():
     s.call(['notify-send', eyeuse_title, eyeuse_message, '--urgency=critical'])
     eyecare()
 
-eyecare_title = 
-eyecare_message = 
+eyecare_title = 'Good job'
+eyecare_message = 'You can get back to work.'
 
 def eyecare():
     time.sleep(22)
-    s.call(['notify-send', 'Good Job', 'You can get back to work.'])
+    s.call(['notify-send', eyecare_title, eyecare_message])
     eyeuse()
 
 eyeuse()
