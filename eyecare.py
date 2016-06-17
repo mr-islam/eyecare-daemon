778accf71b80755b18886c45069250e13f16b614
Naved Islam <navedcoded@gmail.com>
2016-06-17 13:16:05 +0100

    Adding variables for the messages

--- eyecare.py
+++ eyecare.py
@@ -1,11 +1,16 @@
 import subprocess as s
 import time
 
+eyeuse_title = 'Your eyes'
+eyeuse_message = 'Take a break. Just 20 seconds.'
+
 def eyeuse():
     time.sleep(1200)
-    s.call(['notify-send', 'Your Eyes', 'Take a break. Just 20 seconds.', '--urgency=critical'])
+    s.call(['notify-send', eyeuse_title, eyeuse_message, '--urgency=critical'])
     eyecare()
 
+eyecare_title = 
+eyecare_message = 
 
 def eyecare():
     time.sleep(22)
