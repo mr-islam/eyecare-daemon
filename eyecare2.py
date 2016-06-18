4a2bb56c69f4f16431261c5813c432107d402769
NavedCoded <navecoded@gmail.com>
2016-06-18 12:56:37 +0100

    minor syntax fixes

--- eyecare2.py
+++ eyecare2.py
@@ -5,10 +5,10 @@ import platform as p #to detect linux or osx
 os = p.system()
 
 #feel free to change to something more persuasive and personal
-warning_title = 'Your eyes'
-warning_message = 'Take a break. Just 20 seconds.'
-success_title = 'Good job'
-success_message = 'You can get back to work.'
+warning_title = "Your eyes"
+warning_message = "Take a break. Just 20 seconds."
+success_title = "Good job"
+success_message = "You can get back to work."
 
 while os == 'Linux':
     command = 'notify-send' #command being used
@@ -27,10 +27,13 @@ while os == 'Linux':
     s.call([command, priority2, success_title, success_message])
 
 while os == 'Darwin':
-    command = 'display notification' #osx command 
+    command1 = 'osascript -e'
+    command2 = "'display notification" #osx command 
+    success_title = "Good job'"
+    warning_title = "Your eyes'"
 
     time.sleep(4)
-    s.call([command, warning_message, 'with title', warning_title])
+    s.call([command1, warning_message, 'with title', warning_title])
     time.sleep(2)
-    s.call([command, success_message, 'with title', success_message])
+    s.call([command1, success_message, 'with title', success_title])
 
