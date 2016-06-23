8863a2e904bf1cb613555b85c9ddde1fd9f6d79b
NavedCoded <navecoded@gmail.com>
2016-06-23 14:48:02 +0100

    inspiration from the new fork! ;)

--- eyecare.py
+++ eyecare.py
@@ -1,38 +1,42 @@
-import subprocess as s #to pass system commands
-import time  #for pausing the script
-import platform as p #to detect linux or osx
+import subprocess as s  #to pass system commands
+import time as t        #for the timer
+import platform as p    #to detect linux or osx
+
+#feel free to change to something more persuasive and personal
+warning_title =     "Your eyes"
+warning_message =   "Take a break. Just 20 seconds."
+success_title =     "Good job"
+success_message =   "You can get back to work."
+
+#timer_settings
+screen_time =   '1200'
+rest_time =     '22'
 
 os = p.system()
 
-#feel free to change to something more persuasive and personal
-warning_title = "Your eyes"
-warning_message = "Take a break. Just 20 seconds."
-success_title = "Good job"
-success_message = "You can get back to work."
-
-#to specify custom timeout, use `--expire-time=TIME` flag after priority. (TIME in milliseconds)
-
 while os == 'Linux':
-    command = 'notify-send' #command being used
+    command = 'notify-send' #system command
 
-    #notify-send importance parameters. system default is 2.
-    priority1 = '--urgency=low'
-    priority2 = '--urgency=normal'
-    priority3 = '--urgency=critical'
+    #notify-send parameters
+    priority1 =     '--urgency=low'
+    priority2 =     '--urgency=normal'      #system default
+    priority3 =     '--urgency=critical'
+    timeout =       '--expire-time=22'
 
     #the actual script
-    time.sleep(1200)
-    s.call([command, priority3, warning_title, warning_message])
-    time.sleep(22)
-    s.call([command, priority2, success_title, success_message])
+    t.sleep(screen_time)
+    s.call([command, priority3, timeout, warning_title, warning_message])
+    t.sleep(rest_time)
+    s.call([command, priority2, timeout, success_title, success_message])
 
 while os == 'Darwin': #thats OSX
-    command1 = 'osascript -e'
-    command2 = "'display notification" #osx command, extra ' has to be there!
+    command1 =      "osascript -e"
+    command2 =      "'display notification" #osx command, extra ' has to be there!
     success_title = "Good job'" 
-    warning_title = "Your eyes'" #againg, trailing ' is needed!
+    warning_title = "Your eyes'" #again, trailing ' is needed!
 
-    time.sleep(4)
+    time.sleep(4) #short timer to aid in testing
     s.call([command1, warning_message, 'with title', warning_title])
     time.sleep(2)
     s.call([command1, success_message, 'with title', success_title])
+
