2f1a234380fb9cf65eed6ebe72c4e98b7531e6ef
NavedCoded <navecoded@gmail.com>
2016-06-26 21:08:34 +0100

    added `.value` to fix the typeerror from earlier

--- eyecare.py
+++ eyecare.py
@@ -1,22 +1,22 @@
-import subprocess as s  # to pass system commands
-import time  # for pausing the script
-import platform as p  # to detect linux or osx
-from enums import Urgency
+import subprocess as s      # to pass system commands
+import time                 # for pausing the script
+import platform as p        # to detect linux or osx
+from enums import Urgency   # urgency variables
 
 
-# timer settings
-work_time = 1200
+# timer settings in seconds
+work_time = 1200 
 relax_time = 20
 notification_expire_time_ms = 5000  # linux only
 
 # message settings
 warning_title = 'Your eyes'
 warning_message = 'Take a break. Just {} seconds.'.format(relax_time)
-warning_urgency = Urgency.high  # linux only. can be either low, medium or high
+warning_urgency = Urgency.high.value # linux only. can be either low, medium or high
 
 success_title = 'Good job'
 success_message = 'You can get back to work.'
-success_urgency = Urgency.low   # linux only. can be either low, medium or high
+success_urgency = Urgency.low.value  # linux only. can be either low, medium or high
 
 
 # get the system platform
@@ -24,7 +24,7 @@ os = p.system().lower()
 
 
 if os == 'linux':
-    command = 'notify-send'  # command being used
+    command = 'notify-send'  # system command being used
     expiration = '--expire-time={}'.format(notification_expire_time_ms)
 
     while True:
