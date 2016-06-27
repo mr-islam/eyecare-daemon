2c62d37d2924f8c335acbf53847888cbfe3ec355
NavedCoded <navecoded@gmail.com>
2016-06-27 23:32:25 +0100

    os.system option added for linux, osx should work, enums edited for compatibility and readme updated

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 ![Alt text](https://i.imgur.com/B5HF85r.png "Demonstration")
 
-For the system notification, it uses the linux `notify-send` command, and should work with any distro (tested on Arch using Dunst for notifications). Set it to run on startup, and save your eyes! 
+For the system notification, it uses the linux `notify-send` command, and should work with any distro (tested on Arch using Dunst for notifications). Set it to run on startup, and save your eyes!
 
 Now, you can easily change the messages, and also the system command used. Even more fresh: experimental OSX support is here!
+____
+
+# Installation
+
+Simplicity itself.
+
+Download both `eyecare.py` and `enums.py` and place them in the same folder. Set the following code to run at startup (eg. putting it in `xinit.rc` or your window manager config file):
+
+    python ~/path/to/file/eyecare.py
+
+# Dependencies
+
+* python
+* `libnotify` for linux notification (sometimes packaged as `libnotify-bin`)
+* ___ for Windows [coming soon!]
--- eyecare.py
+++ eyecare.py
@@ -2,54 +2,63 @@ import subprocess as s      # to pass system commands
 import time                 # for pausing the script
 import platform as p        # to detect linux or osx
 import os
-#from enums import Urgency   # config
+from enums import Urgency   # config
 
 
 # timer settings
 work_time = 4
 relax_time = 2
-#notification_expire_time_ms = 5000  # linux only
+notification_expire_time_ms = 5000  # linux only
 
 # message settings
 warning_title = 'Your eyes'
 warning_message = 'Take a break. Just {} seconds.'.format(relax_time)
-#warning_urgency = Urgency.high  # linux only. can be either low, medium or high
+warning_urgency = Urgency.high  # linux only. can be either low, medium or high
 
 success_title = 'Good job'
 success_message = 'You can get back to work.'
-#success_urgency = Urgency.low   # linux only. can be either low, medium or high
+success_urgency = Urgency.low   # linux only. can be either low, medium or high
 
 
 # get the system platform
 os1 = p.system().lower()
+
+# 1 is preferred, but try 2 if you encounter an `OSError`
+command_method = 1
 
 
 if os1 == 'linux':
     command = 'notify-send'  # command being used
     expiration = '--expire-time={}'.format(notification_expire_time_ms)
 
-    while True:
+    while command_method == 1:
         time.sleep(work_time)
         s.call([command, warning_urgency, expiration, warning_title, warning_message])
         time.sleep(relax_time)
         s.call([command, success_urgency, expiration, success_title, success_message])
 
+    while command_method == 2:
+        warning_cmd = "{0} {1} {2} \"{3}\" \"{4}\"".format(command,warning_urgency,expiration,warning_title,warning_message)
+        success_cmd = "{0} {1} {2} \"{3}\" \"{4}\"".format(command,success_urgency,expiration,success_title,success_message)
+
+        time.sleep(work_time)
+        os.system(warning_cmd)
+        time.sleep(relax_time)
+        os.system(success_cmd)
+
 if os1 == 'darwin':  # thats OSX
     command1 = 'osascript -e'
     command2 = 'display notification'  # osx command, extra ' has to be there!
     with_title = 'with title'
-#osascript -e 'display notification "Lorem ipsum dolor sit amet" with title "Title"'
-    while True:
+    warning_cmd = "{0} \'{1} \"{2}\" {3} \"{4}\"\'".format(command1,command2,warning_message,with_title,warning_title)
+    success_cmd = "{0} \'{1} \"{2}\" {3} \"{4}\"\'".format(command1,command2,success_message,with_title,success_title)
+
+    while True: #TODO: implement command_method 1 and 2
         time.sleep(work_time)
-        #cmd = "ls -{0} -{1}".format(var1,var2)
-        warning_cmd = "-{0} \'-{1} \"-{2}\" -{3} \"-{4}\"\'".format(command1,command2,warning_message,with_title,warning_title)
         os.system(warning_cmd)
         time.sleep(relax_time)
-        succuess_cmd = "-{0} \'-{1} \"-{2}\" -{3} \"-{4}\"\'".format(command1,command2,success_message,with_title,success_title)
         os.system(success_cmd)
 
+#TODO: add customizability for osx command (http://apple.stackexchange.com/questions/57412/how-can-i-trigger-a-notification-center-notification-from-an-applescript-or-shel/115373#115373)
+#TODO: option between subprocess and os
 #TODO: detect python version, use either `os.system` or `subprocess.call` (http://stackoverflow.com/questions/1093322/how-do-i-check-what-version-of-python-is-running-my-script)
-#TODO: undo linux breakage
-#TODO: fix enum like reddit bro said (https://www.reddit.com/r/osx/comments/4q3aml/anyone_willing_to_help_test_this_python_script/d4py2v3)
-#TODO: add customizability for osx command (http://apple.stackexchange.com/questions/57412/how-can-i-trigger-a-notification-center-notification-from-an-applescript-or-shel/115373#115373) 
-#and also (http://stackoverflow.com/questions/5588064/how-do-i-make-a-mac-terminal-pop-up-alert-applescript)
